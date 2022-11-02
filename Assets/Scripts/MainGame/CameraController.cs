a988f6b445e63bad3157d5942c20a557aac6cfe5
minttea25 <minttea.dev@gmail.com>
2022-11-02 11:15:35 +0900

    added UI Image On All Scene

--- Assets/Scripts/MainGame/CameraController.cs
+++ Assets/Scripts/MainGame/CameraController.cs
@@ -8,7 +8,7 @@ namespace KWY
     public class CameraController : MonoBehaviour
     {
         readonly Vector3 SimulDes = new Vector3(0, 0, -10); // 기본 카메라 위치
-        readonly Vector3 TurnReadyDes = new Vector3(2, -0.3f, -10); // 맵 사이즈에 따라 조정할 필요가 있음 (맵 사이즈에 상대적으로 조정 or 하드 코딩)
+        readonly Vector3 TurnReadyDes = new Vector3(2.3f, -0.3f, -10); // 맵 사이즈에 따라 조정할 필요가 있음 (맵 사이즈에 상대적으로 조정 or 하드 코딩)
 
         float speed = 0.01f;
 
