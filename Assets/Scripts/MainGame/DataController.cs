3461a6dd9cfc0ca74858d475dbf04bd8509a7869
Whislash <101557047+Whislash@users.noreply.github.com>
2022-11-13 20:00:48 +0900

    Temp 23

--- Assets/Scripts/MainGame/DataController.cs
+++ Assets/Scripts/MainGame/DataController.cs
@@ -60,9 +60,11 @@ namespace KWY
                 }
                 else
                 {
+                    Debug.Log("********************" + amount + "********************");
                     GameObject damageTextInstance = PhotonNetwork.Instantiate("TextHolder", pc.Chara.transform.position, Quaternion.identity, 0);
-                    //damageTextInstance.transform.GetChild(0).GetComponent<TextMeshPro>().SetText(amount.ToString());
-                    damageTextInstance.GetPhotonView().GetComponentInChildren<TextMeshPro>().SetText(amount.ToString());
+                    damageTextInstance.transform.GetChild(0).GetComponent<TextMeshPro>().SetText(amount.ToString());
+                    //damageTextInstance.GetPhotonView().GetComponentInChildren<TextMeshPro>().SetText(amount.ToString());
+                    damageTextInstance.transform.GetChild(0).GetComponent<TextMeshPro>().sortingOrder = 999;
                     pc.Chara.DamageHP(-amount);
                 }
             }
