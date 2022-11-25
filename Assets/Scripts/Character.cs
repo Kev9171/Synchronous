bf43d7bbe05d812816ae976eeeb6784e0c7fc96d
Whislash <101557047+Whislash@users.noreply.github.com>
2022-11-25 20:14:44 +0900

    Knight Animation

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -94,6 +94,7 @@ namespace KWY
         private void BreakDownStatus()
         {
             ClearBuff();
+            GetComponent<Animator>().SetBool("IsDead", true);
             GetComponent<SpriteRenderer>().color = Color.red;
 
             if (gameObject.TryGetComponent(out BoxCollider2D c1))
@@ -237,6 +238,18 @@ namespace KWY
             }
             tt += "]";
             return string.Format("CID: {0}, HP: {1}, MP: {2}, Down?: {3}, Buffs: {4}, Observers: {5}", Cb.cid, Hp, Mp, BreakDown, t, tt);
+        }
+
+        [PunRPC]
+        private void SetAnimatorTriggerAttack()
+        {
+            GetComponent<Animator>().SetTrigger("IsAttacking"); // Activates animation.
+        }
+
+        [PunRPC]
+        private void SetAnimatorTriggerJump()
+        {
+            GetComponent<Animator>().SetTrigger("IsJumping"); // Activates animation.
         }
 
         #region IPunObservable implementation
@@ -551,6 +564,7 @@ namespace KWY
         {
             if (BreakDown) return;
 
+            photonView.RPC("SetAnimatorTriggerAttack", RpcTarget.All);
             nowMove = false;
             SkillBase SelSkill = SkillManager.GetData(sid);
 
@@ -583,8 +597,30 @@ namespace KWY
             Debug.LogFormat("{0} / {1} spells {2}", PhotonNetwork.IsMasterClient ? 'M' : 'C', Cb.cid, sid);
         }
 
+        public void SkillSwitcher(SkillBase SelSkill)
+        {
+            switch (SelSkill.sid)
+            {
+                case SID.FireBall:
+                    break;
+                case SID.LightingVolt:
+                    break;
+                case SID.KnightNormal:
+                    //GameObject Arrow = PhotonNetwork.Instantiate("Arrow", transform.position, Quaternion.identity, 0);
+                    //Arrow arrow = GetComponent<Arrow>();
+                    //arrow.targetPosition.x = transform.position.x - 6f;
+                    Debug.Log("Spawn an arrow.");
+                    break;
+                case SID.KnightSpecial:
+                    photonView.RPC("SetAnimatorTriggerJump", RpcTarget.All);
+                    break;
+                default:
+                    break;
+            }
+        }
+
         #endregion
-        
+
 
         #region MonoBehaviour CallBacks
         private void Awake()
@@ -626,19 +662,23 @@ namespace KWY
 
             if (BreakDown) return;
 
-            if (!PhotonNetwork.IsMasterClient)
-            {
-                return;
-            }
+            //if (!PhotonNetwork.IsMasterClient)
+            //{
+            //    return;
+            //}
 
             if (nowMove)
             {
+                GetComponent<Animator>().SetBool("IsMoving", true); // Enables animation of movement.
                 currentTime += Time.deltaTime;
                 if (currentTime >= movementSpeed)
                     currentTime = movementSpeed;
                 transform.position = Vector3.Lerp(gameObject.transform.position, destination, currentTime/movementSpeed);
                 if (transform.position == new Vector3(destination.x, destination.y, 0))
+                {
+                    GetComponent<Animator>().SetBool("IsMoving", false); // Stops movement animation.
                     nowMove = false;
+                }
             }
             else
             {
